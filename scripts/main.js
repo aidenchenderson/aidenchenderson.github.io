9c03b5ebc9e1e4a2b5551668551febbcc5804606
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2024-08-27 19:51:19 -0400

    finished moon animation

--- scripts/main.js
+++ scripts/main.js
@@ -17,12 +17,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
     moonButton.addEventListener("click", function() {
         // add class to fade out overlay and fade in content
         overlay.classList.add("hide");
-        content.classList.add("show");
         
         // remove overlay after fade out completes
         overlay.addEventListener('transitionend', function() {
             overlay.style.display = "none";
             content.style.display = "flex";
+
+            content.offsetHeight;
+
+            content.classList.add("show");
         }, { once: true });
     });
 });
