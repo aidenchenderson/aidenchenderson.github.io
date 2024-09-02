ab1447b29bc1ec91add160e2102df48827d1e6e8
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2024-09-02 15:02:22 -0400

    started js code to make the website single page for less flashing refreshes

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // change the website logo when the user hovers over
     const logoImage = document.getElementById('logoImage');
-
+    
     logoImage.addEventListener('mouseenter', () => {
         logoImage.src = 'images/WebsiteLogo2.png';
     });
@@ -15,18 +15,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const overlay = document.querySelector(".Overlay");
     const content = document.querySelector(".MainContent");
 
-    moonButton.addEventListener("click", function() {
+    moonButton.addEventListener('click', function() {
         // add class to fade out overlay and fade in content
-        overlay.classList.add("hide");
+        overlay.classList.add('hide');
         
         // remove overlay after fade out completes
-        overlay.addEventListener('transitionend', function() {
-            overlay.style.display = "none";
-            content.style.display = "flex";
+        overlay.addEventListener('transitionend', function(){
+            overlay.style.display = 'none';
+            content.style.display = 'flex';
 
             content.offsetHeight;
 
-            content.classList.add("show");
+            content.classList.add('show');
         }, { once: true });
     });
+    // page navigation
+    function navMainPage(){
+        overlay.style.display = 'none';
+        content.classList.add('show');
+        content.style.display = 'flex';
+    }
+
+    logoImage.addEventListener('click', navMainPage);
 });
