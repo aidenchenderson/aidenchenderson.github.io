b81d4137e7b7f9495acaca14bca2669e6fe1daa8
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2024-09-07 18:01:51 -0400

    started working on js for work experience

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 // handle user events
 document.addEventListener('DOMContentLoaded', (event) => {
+    // get elements by their id
+    const jobExpLink = document.getElementById('JobExperienceLinkID');
+    const workExpThumbCont = document.getElementById('WorkExperienceThumbnailContainerID');
+
     // change the website logo when the user hovers over
     const logoImage = document.getElementById('logoImage');
     
@@ -14,10 +18,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const moonButton = document.getElementById("MoonButton");
     const overlay = document.querySelector(".Overlay");
     const content = document.querySelector(".MainContent");
+    const workExp = document.querySelector(".WorkExperienceContent");
 
     moonButton.addEventListener('click', function() {
         // add class to fade out overlay and fade in content
         overlay.classList.add('hide');
+        workExp.classList.add('hide');
         
         // remove overlay after fade out completes
         overlay.addEventListener('transitionend', function(){
@@ -29,12 +35,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
             content.classList.add('show');
         }, { once: true });
     });
+
+    // initialize pages to be hidden
+    
+
     // page navigation
     function navMainPage(){
         overlay.style.display = 'none';
+        workExp.style.display = 'none';
         content.classList.add('show');
         content.style.display = 'flex';
     }
 
+    function navJobPage(){
+        overlay.style.display = 'none';
+        content.style.display = 'none';
+        workExp.classList.add('show');
+        workExp.style.display = 'flex';
+    }
+
+    // check for user input for page navigation
     logoImage.addEventListener('click', navMainPage);
+    jobExpLink.addEventListener('click', navJobPage);
+    workExpThumbCont.addEventListener('click', navJobPage);
 });
