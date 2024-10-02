4cf4e54425a4a2fb1327c8d0841f44f0ed90ac30
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2024-10-01 21:36:35 -0400

    Removed redundant JavaScript code and added comments to CSS. Also included icon for tabs

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // handle user events
 document.addEventListener('DOMContentLoaded', (event) => {
-    // change the website logo when the user hovers over
+    // change the website logo to C++ when the user hovers over
     const logoImage = document.getElementById('logoImage');
     logoImage.addEventListener('mouseenter', () => {
         logoImage.src = 'images/WebsiteLogo2.png';
@@ -17,15 +17,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const skillsThumbCont = document.getElementById('SkillsThumbnailContainer');
     const projectsThumbCont = document.getElementById('ProjectsThumbnailContainer');
     const moonButton = document.getElementById("MoonButton");
+    const popup = document.getElementById('SkillPopup');
+    const popupTitle = document.getElementById('PopupTitle');
+    const popupDescription = document.getElementById('PopupDescription');
+    const popupClose = document.getElementById('ClosePopup');
+
+    // query selector for different parts of the HTML
     const overlay = document.querySelector(".Overlay");
     const content = document.querySelector(".MainContent");
     const workExp = document.querySelector(".WorkExperienceContent");
     const skillsExp = document.querySelector(".SkillsContent");
     const projectsExp = document.querySelector(".ProjectsContent");
-    const popup = document.getElementById('SkillPopup');
-    const popupTitle = document.getElementById('PopupTitle');
-    const popupDescription = document.getElementById('PopupDescription');
-    const popupClose = document.getElementById('ClosePopup');
+    
 
     // function to make the moon button transition to the rest of the website
     moonButton.addEventListener('click', function() {
@@ -48,55 +51,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }, { once: true });
     });
 
-    // page navigation
-    function navMainPage(){
-        overlay.style.display = 'none';
-        workExp.style.display = 'none';
-        skillsExp.style.display = 'none';
-        projectsExp.style.display = 'none';
-        content.classList.add('show');
-        content.style.display = 'flex';
-    }
-
-    function navJobPage(){
-        overlay.style.display = 'none';
-        content.style.display = 'none';
-        skillsExp.style.display = 'none';
-        projectsExp.style.display = 'none';
-        workExp.classList.remove('hide');
-        workExp.classList.add('show');
-        workExp.style.display = 'flex';
-    }
-    
-    function navSkillsPage(){
-        overlay.style.display = 'none';
-        content.style.display = 'none';
-        workExp.style.display = 'none';
-        projectsExp.style.display = 'none';
-        skillsExp.classList.remove('hide');
-        skillsExp.classList.add('show');
-        skillsExp.style.display = 'flex';
-    }
-
-    function navProjectsPage(){
-        overlay.style.display = 'none';
-        content.style.display = 'none';
-        workExp.style.display = 'none';
-        skillsExp.style.display = 'none';
-        projectsExp.classList.remove('hide')
-        projectsExp.classList.add('show');
-        projectsExp.style.display = 'flex';
+    // page navigation function for all navigation
+    function pageNavigation(element){
+        // return a navigation function to fix automatically changing pages problem
+        return function() {
+            // initialize all of the elements to none
+            overlay.style.display = 'none';
+            workExp.style.display = 'none';
+            content.style.display = 'none';
+            skillsExp.style.display = 'none';
+            projectsExp.style.display = 'none';
+            // then show the page that needs to be displayed
+            element.classList.remove('hide');
+            element.classList.add('show');
+            element.style.display = 'flex';
+        };
     }
 
     // check for user input for page navigation
-    logoImage.addEventListener('click', navMainPage);
-    jobExpLink.addEventListener('click', navJobPage);
-    workExpThumbCont.addEventListener('click', navJobPage);
-    skillsThumbCont.addEventListener('click', navSkillsPage);
-    skillsLink.addEventListener('click', navSkillsPage);
-    projectsLink.addEventListener('click', navProjectsPage);
-    projectsThumbCont.addEventListener('click', navProjectsPage);
+    logoImage.addEventListener('click', pageNavigation(content));
+    jobExpLink.addEventListener('click', pageNavigation(workExp));
+    workExpThumbCont.addEventListener('click', pageNavigation(workExp));
+    skillsThumbCont.addEventListener('click', pageNavigation(skillsExp));
+    skillsLink.addEventListener('click', pageNavigation(skillsExp));
+    projectsLink.addEventListener('click', pageNavigation(projectsExp));
+    projectsThumbCont.addEventListener('click', pageNavigation(projectsExp));
 
+    // unique descriptions for the skills.
     const skillDescriptions = {
         "C++": "I use C++ in my free time because of its versatility and power. I enjoy testing my linear algebra and graphics programming skills with tools like OpenGL and CMake. In my class, 'Data Structures and Algorithms 2C03', I use C++ for its speed and its support for both procedural and object-oriented paradigms.",
         "Linux": "I currently use Linux as my 'daily driver' machine. I am familiar with using the Gnome terminal to navigate my way around files, applications, code, and more. As a byproduct of using Linux, I have become familiar with compiling my own C/C++ files using Make files and CMakeLists. I also enjoy using Linux for ease of access programming, open source contributions, and KDE software.",
@@ -117,19 +98,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         popup.style.display = 'flex';
     }
 
+    /* popup closing */
     // close the popup using the X
     popupClose.onclick = function() {
         popup.style.display = 'none';
     }
 
-    // click on the rest of the window anywhere to close the popup
+    // or click on the rest of the window anywhere to close the popup
     window.onclick = function(event) {
         if (event.target === popup) {
             popup.style.display = 'none';
         }
     }
 
-    // Add click event to each skill
+    // add click event to each skill
     const skillItems = document.querySelectorAll('.Skill');
     skillItems.forEach(item => {
         item.addEventListener('click', () => {
@@ -137,6 +119,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
             openPopup(skillName);
         });
     });
-
-
 });
